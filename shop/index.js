5c587f2c8b838d1f91e89eade89a956f7d18da70
Alex-Tkachev <AATkachev@gmail.com>
2016-02-06 16:01:58 +0300

    Almost the end of task.

--- shop/index.js
+++ shop/index.js
@@ -43,19 +43,47 @@ var HelloForm = React.createClass({
 
     },
     render: function () {
+        var self = this;
         return <div>
         {'Hello ' + this.props.name + '!'}
+            <table>
         {this.state.goodsList.map(function (item) {
-            return <div key={item.name}>{item.name}</div>
+            return <tr key={item.name} onClick={self.orderItem.bind(self, item)}><td>{item.name}</td><td>{item.price}</td></tr>
         })}
+                </table>
             <hr/>
+            <table>
+                <th><td>Name</td><td>Price</td><td>Count</td></th>
         {this.state.myOrder.items.map(function (item) {
-            return <div key={item.name}>{item.name}</div>
+            return <tr key={item.name} onClick={self.deleteItem.bind(self, item)}><td>{item.name}</td><td>{item.price}</td><td>{item.count}</td></tr>
         })}
+                </table>
+            <div>Total Price: {this.state.myOrder.totalPrice}</div>
         </div>;
+    },
+    orderItem : function (item) {
+        var self = this;
+        var promise = service.addToOrder(item.vendorCode);
+        promise.then(function(result, response){
+            if(result.code != 200){
+                return
+            }
+            self.setState({myOrder: result.body});
+        })
     },
     getInitialState: function () {
         return {goodsList: [], myOrder: {items: []}}
+
+    },
+    deleteItem : function(item) {
+        var self = this;
+        var promise = service.removeFromOrder(item.vendorCode);
+        promise.then(function(result, response){
+            if(result.code != 200){
+                return
+            }
+            self.setState({myOrder: result.body});
+        })
 
     }
 });
