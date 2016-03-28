79b6b4489263522e8f8c74710ffc98e02685f0ef
Alex-Tkachev <AATkachev@gmail.com>
2016-03-28 11:34:01 +0300

    add basic projects

---
+++ planner/Gruntfile.js
@@ -0,0 +1,115 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+
+    var debugTasks = ['browserify:script_web', 'browserify:script_external', 'cssmin:dist', 'htmlmin:dist'],
+        productionTasks = debugTasks.concat('uglify:dist');
+
+    grunt.registerTask('build-Debug', debugTasks);
+    grunt.registerTask('build-Release', productionTasks);
+    grunt.registerTask('build-Production', productionTasks);
+
+    var externalModules = [
+        "react",
+        "react-dom"
+    ];
+
+    var buildTargetDirectory = 'dist/';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            script_external: {
+                // External modules that don't need to be constantly re-compiled
+                src: ['.'],
+                dest: buildTargetDirectory + 'external.js',
+                options: {
+                    browserifyOptions: {
+                        debug: true
+                    },
+                    alias: externalModules.map(function (mod) {
+                        return mod + ':'
+                    })
+                }
+            },
+            script_web: {
+                options: {
+                    transform: [["babelify", {presets: ["es2015", "stage-0", "react"]}]],
+                    browserifyOptions: {
+                        debug: true
+                    },
+                    external: externalModules
+                },
+                src: 'js/index.js',
+                dest: buildTargetDirectory + 'index.js'
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    sourceMap: true,
+                    sourceMapIncludeSources: true
+                },
+                files: {
+                    //[buildTargetDirectory + 'index.js']: [buildTargetDirectory + 'index.js'],
+                }
+            }
+        },
+        cssmin: {
+            dist: {
+                options: {
+                    keepSpecialComments: 0,
+                    sourceMap: true,
+                    sourceMapInlineSources: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css'],
+                    dest: buildTargetDirectory
+                }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: false,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    src: '*.html',
+                    dest: buildTargetDirectory
+                }]
+            }
+        },
+        watch: {
+            scripts_browser: {
+                files: ['js/**/*.jsx', 'js/**/*.js'],
+                tasks: ['browserify:script_web'],
+                options: {
+                    spawn: false
+                }
+            },
+            styles_browser: {
+                files: ['css/**/*.css'],
+                tasks: ['cssmin'],
+                options: {
+                    spawn: false
+                }
+            },
+            html_browser: {
+                files: ['*.html'],
+                tasks: ['htmlmin'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-browserify");
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+};
---
+++ test/js/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+
+var MainForm = React.createClass({
+    render: function () {
+	return <div>Привет</div>
+    }
+});
+
+ReactDOM.render(
+    <MainForm />,
+    document.getElementById('container')
+);
