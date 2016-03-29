3a18bc1385c3536e75e8250acc9c442440d54608
fridenn <fridenn@tut.by>
2016-03-29 13:27:47 +0300

    New logic.
    Signed-off-by: fridenn <fridenn@tut.by>

--- planner/js/index.js
+++ planner/js/index.js
@@ -1,10 +1,35 @@
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    {AddNewTask} = require('./addNewTask'),
+    {YourTasks} = require('./yourTasks'),
+    {taskService} = require('./taskService');
 
 
 var MainForm = React.createClass({
+    getInitialState: function () {
+        return {mainView: true}
+    },
+    viewAddTasks: function () {
+        this.setState({mainView: false});
+    },
+    onSubmit: function(){
+        this.setState({mainView: true});
+    },
     render: function () {
-	return <div>Hello, plan</div>
+        var view;
+        if(this.state.mainView){
+            view =<div> <YourTasks />
+                <button className="form-element" onClick={this.viewAddTasks}>Add Task</button>
+                </div>;
+        }
+        else{
+            view = <AddNewTask onSubmit={this.onSubmit}/>;
+        } 
+            
+        return <div>
+            {view}
+
+        </div>
     }
 });
 
