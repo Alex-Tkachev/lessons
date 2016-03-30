3af2d5435be03b0eb711eb1930acd7ac873fbabc
gurinovichegor <lych813@mail.ru>
2016-03-30 12:14:12 +0300

    Добавлен выбор задач

--- test/js/taskList.js
+++ test/js/taskList.js
@@ -1,12 +1,19 @@
 var React = require('react'),
-    { tasks } = require('./tasks');
+    {tasks} = require('./tasks');
 
-
+var object = {
+    a: 1
+};
+object.b = 2;
 var TaskList = React.createClass({
+    showText: function (text) {
+        this.props.changeText(text);
+    },
     render: function () {
+        var self = this;
         return <div>
             {tasks.map(function (task, i) {
-                return <div key={"task"+i}>{task.name}</div>
+                return <div onClick={self.showText.bind(self, task.text)} key={"task"+i}>{task.name}</div>
             })}
         </div>
     }
