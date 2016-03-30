ecfb2e858bc50d1871de1b8b48c690d3badc1061
Andrey-Sukharev <andr.sukharev@gmail.com>
2016-03-30 12:46:22 +0400

    parse text and make input

--- test/js/parseString.js
+++ test/js/parseString.js
@@ -1,23 +1,51 @@
-var React = require('react'),
-    ReactDOM = require('react-dom');
+var React = require('react');
 
-var str = "Пример текста в который нужно встав{{и}}ть пропуски. Зайчик бежал по полянке." +
-    " И п{{а}}л в ямку. И {{з}}десь его ждал {{б}}обр, чтобы зак{{о}}пать." +
-    " Но потом он на него {{c}}ел и зайчик здох... И пришел новый. К{{о}}нец.";
-var answers = [];
-//var i
-var j = 0;
-for(i = 0; i < str.length-1; i++) {
-    if (str[i - 1] == '{' && str[i + 1] == '}') {
-        answers[j] = str[i];
-        j = j + 1;
+/* var str = "Пример текста в который нужно встав{и}ть пропуски. Зайчик бежал по полянке." +
+ " И п{а}л в ямку. И {з}десь его ждал {б}обр, чтобы зак{о}пать." +
+ " Но потом он на него {c}ел... И пришел новый. К{о}нец.";
+ */
+
+var ChoiseBox = React.createClass({
+    render: function () {
+        return <input placeholder={this.props.correctAnswer}/>
     }
-    else if (str[i] != '{' && str[i] != '}') {
-    console.log(str[i]);
-}}
+});
 
+function parseString(str) {
+    str = str.replace(/{{/g, '{')
+    str = str.replace(/}}/g, '}')
+    var result = [];
+    var i;
+    var currentStr = '';
+    var isInput = false;
+
+    function addCurrentStr(isNewInput) {
+        if (isInput == true) {
+            result.push(<ChoiseBox key={'box'+i} correctAnswer={currentStr}/>);
+        }
+        else {
+            result.push(currentStr);
+        }
+        isInput = isNewInput;
+        currentStr = '';
+    }
+
+    for (i = 0; i < str.length; i++) {
+        var c = str[i];
+        if (c == '{') {
+            addCurrentStr(true);
+        } else if (c == '}') {
+            addCurrentStr(false);
+        }
+        else {
+            currentStr = currentStr + c;
+        }
+    }
+    addCurrentStr(false);
+
+    return result;
 }
-ReactDOM.render(
-    <MainForm />,
-    document.getElementById('container')
-);
+
+module.exports = {
+    parseString: parseString
+};
