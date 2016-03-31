62c29901f0e1adbeb7c018b41855409f7308a54e
alpetrenko01 <al.petrenko.01@inbox.ru>
2016-03-31 10:50:35 +0300

    add checking answers

--- test/js/taskText.js
+++ test/js/taskText.js
@@ -1,13 +1,43 @@
 var React = require('react'),
     {tasks} = require('./tasks'),
-    {parseString} = require('./parseString');
+    {TaskList} = require('./parseString'),
+    {parseString} = require('./parseString'),
+    {ChoiseBox} = require('./parseString');
 
 
 var TaskText = React.createClass({
+    getInitialState : function () {
+        return {refs : []};
+    },
+    checkAnswers: function () {
+        var count = 0;
+        for(var i = 0; i < this.state.refs.length; i++) {
+          if(!this.refs[this.state.refs[i]].isValid()){
+              count++;
+          }
+        }
+         alert('Количество ваших ошибок: ' + count);
+    },
     render: function () {
-        var text = parseString(this.props.text);
+
+        var children = parseString(this.props.text);
+        this.state.refs = children.refs;
+        //this.refNames = children.refs;
+        //var r = refs[0];
+        //this.refs[r];
+        // var ans = 0;
+        // var j = 1;
+        // for (var i = 0; i < re.length; i++) {
+        //     if (re[i] != children[j].) {
+        //         ans+=1;
+        //     }
+        //     j += 2;
+        // }
+        //
+
         return <div className="task-text">
-            {text}
+            {children.result}
+            <button className="butt" onClick={this.checkAnswers} key='check'>Check</button>
         </div>
     }
 });
