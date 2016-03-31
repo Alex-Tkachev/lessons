9a0c5bc956d135461112bd228178748a73127ba5
Andrey-Sukharev <andr.sukharev@gmail.com>
2016-03-31 14:14:07 +0400

    new task and lesson

---
+++ test/js/taskLesson.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var TaskLesson = React.createClass({
+    render: function () {
+        return <div>Урок</div>
+    }
+});
+
+module.exports = {
+    TaskLesson: TaskLesson
+};
